4fc1ad016d2097a599af8ab9b6c971decb29c0c5
S.Koehler <Steffen.Koehler@rub.de>
2022-08-23 01:20:40 +0200

    feat: script for updating test data

---
+++ scripts/update test files.R
@@ -0,0 +1,14 @@
+rm(list = ls()); devtools::load_all()
+set.seed(270789)
+input_data <- matrix(rnorm(9e4), ncol = 9, nrow = 1e4)
+layer_config <- list(
+  list(number_of_nodes = 5, activation_fun = "relu", bias = TRUE, random_init = NULL),
+  list(number_of_nodes = 2, activation_fun = "relu", bias = TRUE, random_init = NULL),
+  list(number_of_nodes = 1, activation_fun = "sigmoid", bias = TRUE, random_init = NULL)
+)
+ANN <- Network$new(cost_fun = "quadratic", layer_config = layer_config, input_data = input_data, output_data = NULL)
+output_data <- t(ANN$calculate())
+output_data <- (output_data>mean(output_data))*1
+
+save(input_data, output_data, file = "./tests/testthat/test_data/test_network.rdata")
+rm(list = ls())
